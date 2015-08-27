feb5a250df204b8d4f2b06d279b6b98890b0a5ef
Jonathan <jt21091@gmail.com>
2015-08-27 09:56:33 -0400

    Added tag functionality

--- tweets/forms.py
+++ tweets/forms.py
@@ -1,14 +1,20 @@
 from django import forms
 from tweets.models import Article, Category, UserProfile
 from django.core.validators import URLValidator
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout
+from crispy_forms.bootstrap import InlineCheckboxes
 
 class CreateShareForm(forms.ModelForm):
 	
 	share = forms.CharField(required=True, label="", max_length = 140, widget=forms.Textarea(attrs={'style': 'height: 4em;', 'placeholder': 'Add a Quick Comment for Your Article'}))
 	tag = forms.CharField(required=True, label="", widget=forms.widgets.TextInput(attrs={'placeholder': 'Enter a Tag for Your Share'}))
-	category = forms.ModelMultipleChoiceField(required=True, label="", widget=forms.CheckboxSelectMultiple(), queryset=Category.objects.all())
+	category = forms.ModelMultipleChoiceField(required=True, label="", widget=forms.CheckboxSelectMultiple(attrs={'body': 'padding-left: 70px;'}), queryset=Category.objects.all())
 	url = forms.URLField(required = True, label="", widget=forms.URLInput( attrs={'placeholder': 'Article URL'}))
 	
+	helper = FormHelper()
+	helper.layout = Layout(InlineCheckboxes('category'))
+
 	class Meta:
 		model = Article
 		fields =['category', 'share', 'url', 'tag']
