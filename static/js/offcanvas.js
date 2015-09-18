c79b91f8ba6ac5f14676fd6eb63e903f34b90188
Jonathan <jt21091@gmail.com>
2015-09-18 16:42:43 -0500

    Added bootstrap JS cdn

--- static/js/offcanvas.js
+++ static/js/offcanvas.js
@@ -3,3 +3,14 @@ $(document).ready(function () {
     $('.row-offcanvas').toggleClass('active')
   });
 });
+
+$(document).ready(function () {
+  $('.list-group-item').hover(function () {
+    $(this).toggleClass('active')
+  });
+  // $('.list-group-item').mouseleave(function () {
+  //   $(this).removeClass('active')
+  // });
+
+});
+
