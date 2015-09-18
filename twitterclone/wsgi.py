e153b940a4ce9fbd1045cd99805d564bced83650
Jonathan <jt21091@gmail.com>
2015-09-18 17:48:12 -0500

    Reverted whitenoise

--- twitterclone/wsgi.py
+++ twitterclone/wsgi.py
@@ -10,13 +10,13 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+# from whitenoise.django import DjangoWhiteNoise
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "twitterclone.settings")
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+# application = DjangoWhiteNoise(application)
 
 try:
 	from dj_static import Cling
