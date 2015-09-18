1bc374e0e8ad5e1b8a0d71a85a9f9f88dfa81624
Jonathan <jt21091@gmail.com>
2015-09-18 17:44:43 -0500

    Revert "Added bootstrap JS cdn"
    This reverts commit c79b91f8ba6ac5f14676fd6eb63e903f34b90188.

--- static/js/offcanvas.js
+++ static/js/offcanvas.js
@@ -3,14 +3,3 @@ $(document).ready(function () {
     $('.row-offcanvas').toggleClass('active')
   });
 });
-
-$(document).ready(function () {
-  $('.list-group-item').hover(function () {
-    $(this).toggleClass('active')
-  });
-  // $('.list-group-item').mouseleave(function () {
-  //   $(this).removeClass('active')
-  // });
-
-});
-
