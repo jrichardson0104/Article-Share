efe1d10e71953266fed17a9c9e418e0561d640c5
Jonathan <jt21091@gmail.com>
2015-10-27 10:27:15 -0500

    changed databases for testing

--- twitterclone/settings/production.py
+++ twitterclone/settings/production.py
@@ -3,11 +3,11 @@ import os
 
 DEBUG = False
 TEMPLATE_DEBUG = False
-
-
+from django.conf import settings
+DATABASES = settings.DATABASES
 # Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+# import dj_database_url
+# DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
