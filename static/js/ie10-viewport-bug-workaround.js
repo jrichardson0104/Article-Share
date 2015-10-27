7ab3e8c81fd023e75a921445a1124006f731e78d
Jonathan <jt21091@gmail.com>
2015-10-26 22:34:45 -0500

    Revert "added static files"
    This reverts commit 1f2c5cfeac94955a5ebda4a43753712aa7d95cea, reversing
    changes made to 8ce1101e30405fd4e6be87977e42fe40493c165e.

--- static/js/ie10-viewport-bug-workaround.js
+++ static/js/ie10-viewport-bug-workaround.js
@@ -1,13 +1,7 @@
 /*!
  * IE10 viewport hack for Surface/desktop Windows 8 bug
-<<<<<<< HEAD
  * Copyright 2014-2015 Twitter, Inc.
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
-=======
- * Copyright 2014 Twitter, Inc.
- * Licensed under the Creative Commons Attribution 3.0 Unported License. For
- * details, see http://creativecommons.org/licenses/by/3.0/.
->>>>>>> 69c299e4da22fca281499a293237320536e9cfa0
  */
 
 // See the Getting Started docs for more information:
@@ -15,10 +9,7 @@
 
 (function () {
   'use strict';
-<<<<<<< HEAD
 
-=======
->>>>>>> 69c299e4da22fca281499a293237320536e9cfa0
   if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
     var msViewportStyle = document.createElement('style')
     msViewportStyle.appendChild(
@@ -28,8 +19,5 @@
     )
     document.querySelector('head').appendChild(msViewportStyle)
   }
-<<<<<<< HEAD
 
-=======
->>>>>>> 69c299e4da22fca281499a293237320536e9cfa0
 })();
--- twitterclone/wsgi.py
+++ twitterclone/wsgi.py
@@ -11,7 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "twitterclone.settings")
 
 application = get_wsgi_application()
@@ -22,5 +21,3 @@ try:
 	application = Cling(get_wsgi_application())
 except:
 	pass
-
-
